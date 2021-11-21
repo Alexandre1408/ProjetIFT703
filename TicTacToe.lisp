e10c424441ac9429e34720e4a6e4a6ed10d336a0
inga2603 <inga2603@usherbrooke.ca>
2021-11-20 19:01:00 -0500

    phase two

--- TicTacToe.lisp
+++ TicTacToe.lisp
@@ -1,14 +1,38 @@
+
 (clear-all)
 
-(define-model TicTacToe
+(define-model tictactoe
 
-(chunk-type cases line col val)
+(chunk-type line pos case1 case2 case3)
+(chunk-type column pos case1 case2 case3)
+(chunk-type diagonal number case1 case2 case3)
+(chunk-type case numberLine numberCol inDiag1 inDiag2 value)
 
-(chunk-type line line_nb case1 case2 case3) // les cases sont des pointeurs vers les chunk type cases_x_y)
-(chunk-type column col_nb case1 case2 case3)
-(chunk-type diagonal case1 case2 case3)
+(chunk-type searchEmpty lineNumber colNumber)
+(chunk-type playMove bestLineNumber bestColNumber)
 
-(chunk-type play_one matrice state) // state : { playToWin, playToNotLose } 
-// matrice decrit l'état du plateau
+(P test-empty
+   =goal>
+     ISA         searchEmpty
+	 lineNumber  =valueLine
+	 colNumber   =valueCol
+   =imaginal>
+     ISA         case
+     numberLine  =valueLine 
+     numberCol   =valueCol
+	 value 		 "E"
+==>
+   -goal>
+   +goal>
+	ISA		createMove
+	lineNumber =valueLine
+	colNumber =valueCol
+)
+
+(P try-recall-move
+)
+
+(P answer-good-move
+) 
 
 )
